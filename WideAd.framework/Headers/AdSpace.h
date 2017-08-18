37ccea00b80912d7bd3ef8f3b9fd1102a7902092
mjhassan <mjhassan@users.noreply.github.com>
2017-08-18 23:27:21 +0600

    * Uploaded built framework.

---
+++ WideAd.framework/Headers/AdSpace.h
@@ -0,0 +1,19 @@
+//
+//  AdSpace.h
+//  WideAdTest
+//
+//  Created by Jahid on 8/16/17.
+//  Copyright © 2017 Jahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AdSpaceDelegate.h"
+
+@interface AdSpace : UIView
+
+@property (weak, nonatomic) id<AdSpaceDelegate> delegate;
+
+- (void)runAd;
+- (void)closeAd;
+
+@end
---
+++ WideAd.framework/Headers/WideAd.h
@@ -0,0 +1,20 @@
+//
+//  WideAd.h
+//  WideAd
+//
+//  Created by Jahid on 8/16/17.
+//  Copyright © 2017 Jahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WideAd.
+FOUNDATION_EXPORT double WideAdVersionNumber;
+
+//! Project version string for WideAd.
+FOUNDATION_EXPORT const unsigned char WideAdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WideAd/PublicHeader.h>
+
+#import <WideAd/AdSpace.h>
+#import <WideAd/AdSpaceDelegate.h>
